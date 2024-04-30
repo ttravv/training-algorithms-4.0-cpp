a776e81f468544279e8fed794af20dc1eeaa7556
ttravv <161491928+ttravv@users.noreply.github.com>
2024-05-01 01:37:57 +0300

    Add files via upload

---
+++ partition.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int number_elements;
+    cin >> number_elements;
+
+    vector<int> celie_chisla(number_elements);
+    for (int i = 0; i < number_elements; ++i) {
+        cin >> celie_chisla[i];
+    }
+
+    int opornii_elem;
+    cin >> opornii_elem;
+
+    int counter = 0;
+    for (int i : celie_chisla) {
+        if (i < opornii_elem) {
+            counter++;
+        }
+    }
+
+    cout << counter << endl;
+    cout << number_elements - counter << endl;
+
+    return 0;
+}
